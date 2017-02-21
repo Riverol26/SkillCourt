8e2706e00cfe28369e7df28b494b010bba151ec9
riverol26 <riverol26@aol.com>
2017-02-21 17:36:33 -0500

    Initial commit to github - Header, Footer, and routes

---
+++ SkillCourt/routes/about.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.render('about',
+    {
+        site: 'SkillCourt',
+        page: 'About'
+    });
+});
+
+module.exports = router;
---
+++ SkillCourt/routes/download.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.render('download',
+    {
+        site: 'SkillCourt',
+        page: 'Download'
+    });
+});
+
+module.exports = router;
